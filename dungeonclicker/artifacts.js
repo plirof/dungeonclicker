8a141ee594a23c6e10da271b68faf2e81ebcdd3e
PLIROF <plirof@users.noreply.github.com>
2017-02-28 23:44:13 +0200

    v008c4

--- dungeonclicker/artifacts.js
+++ dungeonclicker/artifacts.js
@@ -1,59 +1,59 @@
 dCi["artifacts"] = new Object();
-dCi["artifacts"]["Horseshoe"] = {text:"When you go on an adventure you earn 1 extra coin.", quote:"This item is yet to be identified.", price:"100"};
-dCi["artifacts"]["Rabbit's Foot"] = {text:"When you go on an adventure you earn 1 extra coin.", quote:"This item is yet to be identified.", price:"200"};
-dCi["artifacts"]["Nazar"] = {text:"When you go on an adventure you earn 1 extra coin.", quote:"This item is yet to be identified.", price:"300"};
-dCi["artifacts"]["Lucky Hand"] = {text:"When you go on an adventure you earn 1 extra coin.", quote:"This item is yet to be identified.", price:"400"};
-dCi["artifacts"]["Coin Charm"] = {text:"When you go on an adventure you earn 1 extra coin.", quote:"This item is yet to be identified.", price:"500"};
-dCi["artifacts"]["Golden Rose Clover"] = {text:"When you go on an adventure you earn 5 extra coin.", quote:"This golden five-leaf clover trinket is said to bring luck and prosperity to its owner.", price:"1000"};
+dCi["artifacts"]["Horseshoe"] = {text:translate("When you go on an adventure you earn 1 extra coin."), quote:translate("This item is yet to be identified."), price:"100"};
+dCi["artifacts"]["Rabbit's Foot"] = {text:translate("When you go on an adventure you earn 1 extra coin."), quote:translate("This item is yet to be identified."), price:"200"};
+dCi["artifacts"]["Nazar"] = {text:translate("When you go on an adventure you earn 1 extra coin."), quote:translate("This item is yet to be identified."), price:"300"};
+dCi["artifacts"]["Lucky Hand"] = {text:translate("When you go on an adventure you earn 1 extra coin."), quote:translate("This item is yet to be identified."), price:"400"};
+dCi["artifacts"]["Coin Charm"] = {text:translate("When you go on an adventure you earn 1 extra coin."), quote:translate("This item is yet to be identified."), price:"500"};
+dCi["artifacts"]["Golden Rose Clover"] = {text:translate("When you go on an adventure you earn 5 extra coin."), quote:translate("This golden five-leaf clover trinket is said to bring luck and prosperity to its owner."), price:"1000"};
 
-//dCi["artifacts"]["Animal Messenger"] = {text:"Allows you to buy buildings and artifacts directly from the Adventure panel.", quote:"This small magical beast carries messages between you and your agents through long distances.",price:"1000"};
+//dCi["artifacts"]["Animal Messenger"] = {text:translate("Allows you to buy buildings and artifacts directly from the Adventure panel."), quote:translate("This small magical beast carries messages between you and your agents through long distances."),price:"1000"};
 
-dCi["artifacts"]["Alchemist's Kit"] = {text:"Increses your income by 33.", quote:"This set of primitive alchemy tools allows you to turn small quantities of lead into silver.",price:"3333"};
-dCi["artifacts"]["Philosophic Mercury"] = {text:"Increses your income by 33,333.", quote:"This magical substance allows you to slowly and laboriously turn small quantities of lead into gold. The process can take up to 1 year.",price:"3333333"};
-dCi["artifacts"]["Philosopher's Stone"] = {text:"Increses your income by 33,333,333.", quote:"This mystical substance allows you to instantly turn any quantity of lead into gold. It also makes you immortal.",price:"3333333333"};
+dCi["artifacts"]["Alchemist's Kit"] = {text:translate("Increases your income by ")+"33.", quote:translate("This set of primitive alchemy tools allows you to turn small quantities of lead into silver."),price:"3333"};
+dCi["artifacts"]["Philosophic Mercury"] = {text:translate("Increases your income by ")+"33,333.", quote:translate("This magical substance allows you to slowly and laboriously turn small quantities of lead into gold. The process can take up to 1 year."),price:"3333333"};
+dCi["artifacts"]["Philosopher's Stone"] = {text:translate("Increases your income by ")+"33,333,333.", quote:translate("This mystical substance allows you to instantly turn any quantity of lead into gold. It also makes you immortal."),price:"3333333333"};
 
-dCi["artifacts"]["Verdant Bead of the Windwalker"] = {text:"When you go on an adventure you earn extra coin equal to 0.5% to 1% of your current income, rounded up.", quote:"This item is yet to be identified.",price:"5000"};
-dCi["artifacts"]["Amber Bead of the Sandwalker"] = {text:"When you go on an adventure you earn extra coin equal to 0.5% to 1% of your current income, rounded up.", quote:"This item is yet to be identified.",price:"50000"};
-dCi["artifacts"]["Crimson Bead of the Flamewalker"] = {text:"When you go on an adventure you earn extra coin equal to 1% to 2.5% of your current income, rounded up.", quote:"This item is yet to be identified.",price:"500000"};
-dCi["artifacts"]["Azure Bead of the Rainwalker"] = {text:"When you go on an adventure you earn extra coin equal to 1% to 2.5% of your current income, rounded up.", quote:"This item is yet to be identified.",price:"5000000"};
+dCi["artifacts"]["Verdant Bead of the Windwalker"] = {text:translate("When you go on an adventure you earn extra coin equal to")+" 0.5%"+translate(" to ")+"1% "+translate("of your current income, rounded up."), quote:translate("This item is yet to be identified."),price:"5000"};
+dCi["artifacts"]["Amber Bead of the Sandwalker"] = {text:translate("When you go on an adventure you earn extra coin equal to")+" 0.5%"+translate(" to ")+"1% "+translate("of your current income, rounded up."), quote:translate("This item is yet to be identified."),price:"50000"};
+dCi["artifacts"]["Crimson Bead of the Flamewalker"] = {text:translate("When you go on an adventure you earn extra coin equal to")+" 1%"+translate(" to ")+"2.5% "+translate("of your current income, rounded up."), quote:translate("This item is yet to be identified."),price:"500000"};
+dCi["artifacts"]["Azure Bead of the Rainwalker"] = {text:translate("When you go on an adventure you earn extra coin equal to")+" 1%"+translate(" to ")+"2.5% "+translate("of your current income, rounded up."), quote:translate("This item is yet to be identified."),price:"5000000"};
 
-dCi["artifacts"]["Boots of Alacrity"] = {text:"Each time you click on the <em>adventure!</em> button, you go on an adventure twice.", quote:"This tattered pair of dragonhide boots allows its wearer to cross long distances in the blink of an eye.", price:"25000"};
+dCi["artifacts"]["Boots of Alacrity"] = {text:translate("Each time you click on the <em>adventure!</em> button, you go on an adventure twice."), quote:translate("This tattered pair of dragonhide boots allows its wearer to cross long distances in the blink of an eye."), price:"25000"};
 
-dCi["artifacts"]["Book of Aresius"] = {text:"You unlock 8 heroic adventures. Heroic adventures are extremely rare, but they give 10 times more coin.", quote:"&laquo;Remember, proud brothers: everything is possible when you let the mystic power of the Dragonflame burn in your heart.&raquo;<br />&ndash; Aresius", price:"1000000"};
-dCi["artifacts"]["Book of Antonidas"] = {text:"You unlock 8 heroic adventures. Heroic adventures are extremely rare, but they give 10 times more coin.", quote:"&laquo;This is not for idle hands, nor prying eyes. Information must not be lost. But it must not be used unwisely.&raquo;<br />&ndash; Archmage Antonidas", price:"1000000"};
+dCi["artifacts"]["Book of Aresius"] = {text:translate("You unlock 8 heroic adventures. Heroic adventures are extremely rare, but they give 10 times more coin."), quote:translate("&laquo;Remember, proud brothers: everything is possible when you let the mystic power of the Dragonflame burn in your heart.&raquo;<br />&ndash; Aresius"), price:"1000000"};
+dCi["artifacts"]["Book of Antonidas"] = {text:translate("You unlock 8 heroic adventures. Heroic adventures are extremely rare, but they give 10 times more coin."), quote:translate("&laquo;This is not for idle hands, nor prying eyes. Information must not be lost. But it must not be used unwisely.&raquo;<br />&ndash; Archmage Antonidas"), price:"1000000"};
 
-dCi["artifacts"]["Brooch of the Prosperous Adventurer"] = {text:"When you go on an adventure you earn 5% extra coin.", quote:"This item is yet to be identified.", price:"500000"};
-dCi["artifacts"]["Pendant of the Prosperous Adventurer"] = {text:"When you go on an adventure you earn 10% extra coin.", quote:"This item is yet to be identified.", price:"5000000"};
-dCi["artifacts"]["Necklace of the Prosperous Adventurer"] = {text:"When you go on an adventure you earn 15% extra coin.", quote:"This item is yet to be identified.", price:"50000000"};
-dCi["artifacts"]["Gloves of the Prosperous Adventurer"] = {text:"When you go on an adventure you earn 20% extra coin.", quote:"This item is yet to be identified.", price:"500000000"};
-dCi["artifacts"]["Cape of the Prosperous Adventurer"] = {text:"When you go on an adventure you earn 25% extra coin.", quote:"This fine silken cape has a small embroidery hidden on the inside that reads: \"Money goes where money is\".", price:"5000000000"};
+dCi["artifacts"]["Brooch of the Prosperous Adventurer"] = {text:translate("When you go on an adventure you earn")+" 5% "+translate("extra coin."), quote:translate("This item is yet to be identified."), price:"500000"};
+dCi["artifacts"]["Pendant of the Prosperous Adventurer"] = {text:translate("When you go on an adventure you earn")+" 10% "+translate("extra coin."), quote:translate("This item is yet to be identified."), price:"5000000"};
+dCi["artifacts"]["Necklace of the Prosperous Adventurer"] = {text:translate("When you go on an adventure you earn")+" 15% "+translate("extra coin."), quote:translate("This item is yet to be identified."), price:"50000000"};
+dCi["artifacts"]["Gloves of the Prosperous Adventurer"] = {text:translate("When you go on an adventure you earn")+" 20% "+translate("extra coin."), quote:translate("This item is yet to be identified."), price:"500000000"};
+dCi["artifacts"]["Cape of the Prosperous Adventurer"] = {text:translate("When you go on an adventure you earn")+" 25% "+translate("extra coin."), quote:translate("This fine silken cape has a small embroidery hidden on the inside that reads: \"Money goes where money is\"."), price:"5000000000"};
 
-dCi["artifacts"]["Talon of the Harpy"] = {text:"Increases your income by 1%", quote:"This item is yet to be identified.", price:"10000"};
-dCi["artifacts"]["Eye of the Cockatrice"] = {text:"Increases your income by 2%", quote:"This item is yet to be identified.", price:"100000"};
-dCi["artifacts"]["Sting of the Wyvern"] = {text:"Increases your income by 3%", quote:"This item is yet to be identified.", price:"1000000"};
-dCi["artifacts"]["Claw of the Sphynx"] = {text:"Increases your income by 4%", quote:"This item is yet to be identified.", price:"10000000"};
+dCi["artifacts"]["Talon of the Harpy"] = {text:translate("Increases your income by  ")+"1%", quote:translate("This item is yet to be identified."), price:"10000"};
+dCi["artifacts"]["Eye of the Cockatrice"] = {text:translate("Increases your income by  ")+"2%", quote:translate("This item is yet to be identified."), price:"100000"};
+dCi["artifacts"]["Sting of the Wyvern"] = {text:translate("Increases your income by  ")+"3%", quote:translate("This item is yet to be identified."), price:"1000000"};
+dCi["artifacts"]["Claw of the Sphynx"] = {text:translate("Increases your income by  ")+"4%", quote:translate("This item is yet to be identified."), price:"10000000"};
 
-dCi["artifacts"]["Left Horn of Mammorus"] = {text:"Increases your income by 5%", quote:"This item is yet to be identified.", price:"100000000"};
-dCi["artifacts"]["Right Horn of Mammorus"] = {text:"Increases your income by 5%", quote:"This item is yet to be identified.", price:"100000000"};
+dCi["artifacts"]["Left Horn of Mammorus"] = {text:translate("Increases your income by  ")+"5%", quote:translate("This item is yet to be identified."), price:"100000000"};
+dCi["artifacts"]["Right Horn of Mammorus"] = {text:translate("Increases your income by  ")+"5%", quote:translate("This item is yet to be identified."), price:"100000000"};
 
-dCi["artifacts"]["Scale of the Drake"] = {text:"Increases your income by 6%", quote:"This item is yet to be identified.", price:"1000000000"};
-dCi["artifacts"]["Spike of the Basilisk"] = {text:"Increases your income by 7%", quote:"This item is yet to be identified.", price:"10000000000"};
-dCi["artifacts"]["Tooth of the Manticore"] = {text:"Increases your income by 8%", quote:"This item is yet to be identified.", price:"100000000000"};
-dCi["artifacts"]["Heart of the Kraken"] = {text:"Increases your income by 9%", quote:"This item is yet to be identified.", price:"1000000000000"};
-dCi["artifacts"]["Left Tusk of the Tarrasque"] = {text:"Increases your income by 10%", quote:"This item is yet to be identified.", price:"10000000000000"};
-dCi["artifacts"]["Right Tusk of the Tarrasque"] = {text:"Increases your income by 10%", quote:"This item is yet to be identified.", price:"10000000000000"};
+dCi["artifacts"]["Scale of the Drake"] = {text:translate("Increases your income by ")+"6%", quote:translate("This item is yet to be identified."), price:"1000000000"};
+dCi["artifacts"]["Spike of the Basilisk"] = {text:translate("Increases your income by  ")+"7%", quote:translate("This item is yet to be identified."), price:"10000000000"};
+dCi["artifacts"]["Tooth of the Manticore"] = {text:translate("Increases your income by  ")+"8%", quote:translate("This item is yet to be identified."), price:"100000000000"};
+dCi["artifacts"]["Heart of the Kraken"] = {text:translate("Increases your income by  ")+"9%", quote:translate("This item is yet to be identified."), price:"1000000000000"};
+dCi["artifacts"]["Left Tusk of the Tarrasque"] = {text:translate("Increases your income by  ")+"10%", quote:translate("This item is yet to be identified."), price:"10000000000000"};
+dCi["artifacts"]["Right Tusk of the Tarrasque"] = {text:translate("Increases your income by  ")+"10%", quote:translate("This item is yet to be identified."), price:"10000000000000"};
 
-dCi["artifacts"]["Ring of Luck"] = {text:"Doubles your income.", quote:"This ancient golden ring found its way to the finger of many a successful adventurer.", price:"7777777"};
+dCi["artifacts"]["Ring of Luck"] = {text:translate("Doubles your income."), quote:translate("This ancient golden ring found its way to the finger of many a successful adventurer."), price:"7777777"};
 
 /*
-dCi["artifacts"]["Bracers of Lightning Slashes"] = {text:"When you hit a monster you have a 10% chance of inflicting a critical hit, dealing double damage.", quote:"Weapons feel almost weightless while wearing this pair of steel bracers.", price:"100000"};
-dCi["artifacts"]["Ruby of Wicked Wounds"] = {text:"When you leave a dungeon, your foes don't recover HP.", quote:"When inserted in the hilt of a weapon, this glowing gem turns warm to touch. Wounds made with such a weapon can't be healed by nonmagical means.", price:"1000000"};
-dCi["artifacts"]["Pendants of Dicernment"] = {text:"You earn double experience.", quote:"Unavailable", price:"999999999999999"};
-dCi["artifacts"]["Tome of Practical Wisdom"] = {text:"You earn more experience for each achievement you unlock.", quote:"Unavailable", price:"999999999999999"};
-dCi["artifacts"]["Crown of the Dwarven King"] = {text:"You earn double coin.", quote:"Unavailable", price:"999999999999999"};
-dCi["artifacts"]["Tome of Provident Planification"] = {text:"You earn more coin for each achievement you unlock.", quote:"Unavailable", price:"999999999999999"};
-dCi["artifacts"]["Gauntlets of Titan Strength"] = {text:"You deal double damage.", quote:"Unavailable", price:"999999999999999"};
-dCi["artifacts"]["Tome of True Strikes"] = {text:"You deal more damage for each achievement you unlock.", quote:"Unavailable", price:"999999999999999"};
+dCi["artifacts"]["Bracers of Lightning Slashes"] = {text:translate("When you hit a monster you have a 10% chance of inflicting a critical hit, dealing double damage."), quote:translate("Weapons feel almost weightless while wearing this pair of steel bracers."), price:"100000"};
+dCi["artifacts"]["Ruby of Wicked Wounds"] = {text:translate("When you leave a dungeon, your foes don't recover HP."), quote:translate("When inserted in the hilt of a weapon, this glowing gem turns warm to touch. Wounds made with such a weapon can't be healed by nonmagical means."), price:"1000000"};
+dCi["artifacts"]["Pendants of Dicernment"] = {text:translate("You earn double experience."), quote:translate("Unavailable"), price:"999999999999999"};
+dCi["artifacts"]["Tome of Practical Wisdom"] = {text:translate("You earn more experience for each achievement you unlock."), quote:translate("Unavailable"), price:"999999999999999"};
+dCi["artifacts"]["Crown of the Dwarven King"] = {text:translate("You earn double coin."), quote:translate("Unavailable"), price:"999999999999999"};
+dCi["artifacts"]["Tome of Provident Planification"] = {text:translate("You earn more coin for each achievement you unlock."), quote:translate("Unavailable"), price:"999999999999999"};
+dCi["artifacts"]["Gauntlets of Titan Strength"] = {text:translate("You deal double damage."), quote:translate("Unavailable"), price:"999999999999999"};
+dCi["artifacts"]["Tome of True Strikes"] = {text:translate("You deal more damage for each achievement you unlock."), quote:translate("Unavailable"), price:"999999999999999"};
 */
 
 dC["artifacts"] = new Object();
@@ -106,7 +106,7 @@ function initArtifactsPanel() {
 
 		spawnParticles("coin", 5);
 		
-		toLog("<span class='notification'>" + key + " purchased!</span>");
+		toLog("<span class='notification'>" + translate(key) + " "+translate("purchased!")+"</span>");
 		$(this).mouseenter();
 	});
 }
@@ -158,7 +158,7 @@ function updateArtifactsNotificationStatus() {
 function updateArtifacts() {
 	for (var key in dCi["artifacts"]) {
 		if (dC["artifacts"][key] == true) {
-			$(".element-price", '#artifacts .element[data-key="' + key + '"]').html("Purchased");
+			$(".element-price", '#artifacts .element[data-key="' + key + '"]').html(translate("Purchased"));
 			$(".element-quantity", '#artifacts .element[data-key="' + key + '"]').html("&#10003;");
 			buffs[key] = true;
 			continue;
@@ -171,6 +171,6 @@ function updateArtifacts() {
 		else {
 			price = price.addCommas();
 		}
-		$(".element-price", '#artifacts .element[data-key="' + key + '"]').html("<span class='coin'>" + price + "<span class='symbol'>g</span> <span class='purchase'>purchase!</span></span>");
+		$(".element-price", '#artifacts .element[data-key="' + key + '"]').html("<span class='coin'>" + price + "<span class='symbol'>g</span> <span class='purchase'>"+translate("purchase!")+"</span></span>");
 	}
 }
